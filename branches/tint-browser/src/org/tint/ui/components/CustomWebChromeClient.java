d05aaa0b84241c9b62e34371af11b2da19b7452e
J.Devauchelle <J.Devauchelle@e747efee-954d-ea6f-70a2-271fa72c5eea>
2011-03-14 11:01:39 +0000

    git-svn-id: http://zirco-browser.googlecode.com/svn@268 e747efee-954d-ea6f-70a2-271fa72c5eea

--- branches/tint-browser/src/org/tint/ui/components/CustomWebChromeClient.java
+++ branches/tint-browser/src/org/tint/ui/components/CustomWebChromeClient.java
@@ -7,12 +7,19 @@ import org.tint.runnables.HistoryUpdaterRunnable;
 import org.tint.ui.IWebViewActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.os.Message;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Custom WebChromeClient implementation.
@@ -71,4 +78,84 @@ public class CustomWebChromeClient extends WebChromeClient {
 		
 		super.onReceivedIcon(view, icon);
 	}
+
+	@Override
+	public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+		new AlertDialog.Builder(mMainActivity)
+		.setTitle(R.string.Commons_JavaScriptAlert)
+		.setMessage(message)
+		.setPositiveButton(android.R.string.ok,
+				new AlertDialog.OnClickListener()
+		{
+			public void onClick(DialogInterface dialog, int which) {
+				result.confirm();
+			}
+		})
+		.setCancelable(false)
+		.create()
+		.show();
+
+		return true;
+	}
+	
+	@Override
+	public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
+		new AlertDialog.Builder(mMainActivity)
+		.setTitle(R.string.Commons_JavaScriptConfirm)
+		.setMessage(message)
+		.setPositiveButton(android.R.string.ok, 
+				new DialogInterface.OnClickListener() 
+		{
+			public void onClick(DialogInterface dialog, int which) {
+				result.confirm();
+			}
+		})
+		.setNegativeButton(android.R.string.cancel, 
+				new DialogInterface.OnClickListener() 
+		{
+			public void onClick(DialogInterface dialog, int which) {
+				result.cancel();
+			}
+		})
+		.create()
+		.show();
+
+		return true;
+	}
+
+	@Override
+	public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, final JsPromptResult result) {
+		final LayoutInflater factory = LayoutInflater.from(mMainActivity);
+        final View v = factory.inflate(R.layout.javascript_prompt_dialog, null);
+        ((TextView) v.findViewById(R.id.JavaScriptPromptMessage)).setText(message);
+        ((EditText) v.findViewById(R.id.JavaScriptPromptInput)).setText(defaultValue);
+
+        new AlertDialog.Builder(mMainActivity)
+            .setTitle(R.string.Commons_JavaScriptPrompt)
+            .setView(v)
+            .setPositiveButton(android.R.string.ok,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            String value = ((EditText) v.findViewById(R.id.JavaScriptPromptInput)).getText()
+                                    .toString();
+                            result.confirm(value);
+                        }
+                    })
+            .setNegativeButton(android.R.string.cancel,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            result.cancel();
+                        }
+                    })
+            .setOnCancelListener(
+                    new DialogInterface.OnCancelListener() {
+                        public void onCancel(DialogInterface dialog) {
+                            result.cancel();
+                        }
+                    })
+            .show();
+        
+        return true;
+	}
+	
 }
