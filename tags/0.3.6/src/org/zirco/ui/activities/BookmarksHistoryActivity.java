c9ed41da3c0450063bcc56d35f3e2a2ff5cbe533
j.Devauchelle <j.Devauchelle@e747efee-954d-ea6f-70a2-271fa72c5eea>
2011-04-02 10:52:20 +0000

    Create tag 0.3.6 for release.
    git-svn-id: http://zirco-browser.googlecode.com/svn@318 e747efee-954d-ea6f-70a2-271fa72c5eea

---
+++ tags/0.3.6/src/org/zirco/ui/activities/BookmarksHistoryActivity.java
@@ -0,0 +1,75 @@
+/*
+ * Zirco Browser for Android
+ * 
+ * Copyright (C) 2010 J. Devauchelle and contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 3 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package org.zirco.ui.activities;
+
+import org.zirco.R;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+
+/**
+ * Combined bookmarks and history activity.
+ */
+public class BookmarksHistoryActivity extends TabActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.bookmarkshistoryactivity);
+		
+		setTitle(R.string.BookmarksListActivity_Title);
+		
+		Resources res = getResources();
+		TabHost tabHost = getTabHost();
+		TabHost.TabSpec spec;
+		Intent intent;
+		
+		// Bookmarks
+		intent = new Intent().setClass(this, BookmarksListActivity.class);
+		
+		spec = tabHost.newTabSpec("bookmarks").setIndicator(res.getString(R.string.Main_MenuShowBookmarks),
+                res.getDrawable(R.drawable.ic_tab_bookmarks))
+                .setContent(intent);
+		tabHost.addTab(spec);
+		
+		// History
+		intent = new Intent().setClass(this, HistoryListActivity.class);
+
+		spec = tabHost.newTabSpec("history").setIndicator(res.getString(R.string.Main_MenuShowHistory),
+                res.getDrawable(R.drawable.ic_tab_history))
+                .setContent(intent);
+		tabHost.addTab(spec);
+		
+		tabHost.setCurrentTab(0);
+		
+		tabHost.setOnTabChangedListener(new OnTabChangeListener() {			
+			@Override
+			public void onTabChanged(String tabId) {
+				if (tabId.equals("bookmarks")) {
+					setTitle(R.string.BookmarksListActivity_Title);
+				} else if (tabId.equals("history")) {
+					setTitle(R.string.HistoryListActivity_Title);
+				} else {
+					setTitle(R.string.ApplicationName);
+				}
+			}
+		});
+	}
+}
