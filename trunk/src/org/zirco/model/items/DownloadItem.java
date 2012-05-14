a1e845e313dc93fcca80b4664898bb67a5c7d0b6
j.Devauchelle <j.Devauchelle@e747efee-954d-ea6f-70a2-271fa72c5eea>
2012-05-14 18:18:29 +0000

    Open downloaded files. Fix issue #57.
    git-svn-id: http://zirco-browser.googlecode.com/svn@462 e747efee-954d-ea6f-70a2-271fa72c5eea

--- trunk/src/org/zirco/model/items/DownloadItem.java
+++ trunk/src/org/zirco/model/items/DownloadItem.java
@@ -15,6 +15,7 @@
 
 package org.zirco.model.items;
 
+import java.io.File;
 import java.util.Random;
 
 import org.zirco.R;
@@ -22,6 +23,7 @@ import org.zirco.events.EventConstants;
 import org.zirco.events.EventController;
 import org.zirco.ui.activities.DownloadsListActivity;
 import org.zirco.ui.runnables.DownloadRunnable;
+import org.zirco.utils.IOUtils;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -97,6 +99,10 @@ public class DownloadItem {
 	 */
 	public String getFileName() {
 		return mFileName;
+	}
+	
+	public String getFilePath() {
+		return IOUtils.getDownloadFolder().getAbsolutePath() + File.separator + mFileName;
 	}
 	
 	/**
